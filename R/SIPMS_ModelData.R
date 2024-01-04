d937161dd8e086a606bf5d68b13afd97f55b5b55
Hassan Gharibi <43888767+hassanakthv@users.noreply.github.com>
2024-01-04 10:04:36 +0100

    Create SIPMS_ModelData.R

---
+++ R/SIPMS_ModelData.R
@@ -0,0 +1,8 @@
+#' Dataset and Prediction model for Species Identification as of 2024-01-04
+#'
+#' The SIPMS_ModelData.RData comprise of the species peptides marker, informative peptides and the random forest prediction model. 
+#'
+#' @docType data
+#' @name SIPMS_ModelData
+#' @usage data(your_data)
+NULL
